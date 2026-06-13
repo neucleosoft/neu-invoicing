346d4e974666c045be7ac62c7b266aff205b94e6
Himanshu-Yadav897 <hy897433@gmail.com>
2026-06-13 20:26:46 +0530

    chore(mobile): register 0002 deletedAt migration

--- apps/mobile/drizzle/migrations.js
+++ apps/mobile/drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_lyrical_scarlet_witch.sql';
 import m0001 from './0001_swift_psynapse.sql';
+import m0002 from './0002_romantic_piledriver.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
